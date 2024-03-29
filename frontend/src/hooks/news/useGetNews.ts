a998df04c797a37552bbb87351b0e842f6b196a7
owbird <owbirdphorccuo@gmail.com>
2024-03-29 22:02:16 +0000

    chore (News): Do not refetch on window focus

--- frontend/src/hooks/news/useGetNews.ts
+++ frontend/src/hooks/news/useGetNews.ts
@@ -2,10 +2,15 @@ import { NewsResponse } from "@/types/news";
 import { useQuery } from "react-query";
 
 const useNewsFunctions = () => {
-	const { isLoading, error, data } = useQuery("news", () =>
-		fetch("http://localhost:8080/api/v1/news/").then(
-			(res) => res.json() as Promise<NewsResponse>,
-		),
+	const { isLoading, error, data } = useQuery(
+		"news",
+		() =>
+			fetch("http://localhost:8080/api/v1/news/").then(
+				(res) => res.json() as Promise<NewsResponse>,
+			),
+		{
+			refetchOnWindowFocus: false,
+		},
 	);
 
 	return { isLoading, error, data };
