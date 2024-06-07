25c0062732a25730a8c82d35c3e3d94ef36e2637
owbird <owbirdphorccuo@gmail.com>
2024-06-07 06:40:05 +0000

    feat: Embed AIM API server

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"os/exec"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -11,17 +12,23 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
+//go:embed aim-api-server
+var aimServer embed.FS
+
 func main() {
 	// Create an instance of the app structure
 	app := NewApp()
 
+	// Start the API server
+	exec.Command("./aim-api-server").Start()
+
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "AIM",
-		Width:  1024,
-		Height: 768,
+		Title:     "AIM",
+		Width:     1024,
+		Height:    768,
 		MinHeight: 768,
-		MinWidth: 1024,
+		MinWidth:  1024,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
@@ -30,7 +37,6 @@ func main() {
 			app,
 		},
 	})
-
 	if err != nil {
 		println("Error:", err.Error())
 	}
